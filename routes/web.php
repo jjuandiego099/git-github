d87704d3e5196c82e244b443d0aab24d8815d4c4
jjuandiego099 <jchaparro167@unab.edu.co>
2025-09-04 11:20:04 -0500

    funciones

--- routes/web.php
+++ routes/web.php
@@ -4,25 +4,32 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     echo "oli";
-    $name="fabian Suarez";
-    $age=rand(18,40);
-    $islogin=true;
-    $height=1.86;
-    $message="";
+    $name = "fabian Suarez";
+    $age = rand(18, 40);
+    $islogin = true;
+    $height = 1.86;
+    $message = "";
     echo $name;
-    if($age>=18){
-        $message .="eres mayor de edad";
-
-    }else{
-        $message .="No eres mayor de edad";
-
-        
+    if ($age >= 18) {
+        $message .= "eres mayor de edad";
+    } else {
+        $message .= "No eres mayor de edad";
     }
-    $message.="".($islogin? "Ya estas logueado":"No estas logueado");
+    callback($age, function ($edad) {
+        echo "hola tengo $edad de edad";
+    });
+
+    $message .= "" . ($islogin ? "Ya estas logueado" : "No estas logueado");
     echo "<br>";
     echo $message;
     echo "<br>";
-    
-
-
+    echo printUser($name);
 });
+function printUser($name)
+{
+    return "El nombre del usuario es $name";
+}
+function callback($age, $callable)
+{
+    $callable($age);
+}
