c63638999a6021dce38ba53dc6d1ec50222c26da
jjuandiego099 <jchaparro167@unab.edu.co>
2025-09-04 11:09:53 -0500

    estructuras

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,21 @@ Route::get('/', function () {
     $age=rand(18,40);
     $islogin=true;
     $height=1.86;
+    $message="";
     echo $name;
+    if($age>=18){
+        $message .="eres mayor de edad";
+
+    }else{
+        $message .="No eres mayor de edad";
+
+        
+    }
+    $message.="".($islogin? "Ya estas logueado":"No estas logueado");
+    echo "<br>";
+    echo $message;
+    echo "<br>";
+    
 
 
 });
