0f34b0fcb3338fa6d8b582130216532c86e296c1
jjuandiego099 <jchaparro167@unab.edu.co>
2025-09-04 10:56:29 -0500

    variables

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "oli";
+    $name="fabian Suarez";
+    $age=rand(18,40);
+    $islogin=true;
+    $height=1.86;
+    echo $name;
+
+
 });
